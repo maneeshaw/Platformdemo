4ad98397306dc4cfbe71c85500c7d296d4c354d8
Maneesha Wijesinghe <maneesha.wijesinghe@maneeshademo.com>
2016-11-28 01:59:50 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+
+var request = require("request");
 var router = express.Router();
 
 /* GET home page. */
@@ -15,7 +17,26 @@ router.get('/registration', function(req, res, next) {
 });
 
 router.get('/applications', function(req, res, next) {
-  res.render('applications', { title: 'Applications' });
+  var options = { method: 'GET',
+  url: 'https://forest.okta.com/api/v1/users/me/appLinks',
+  headers: 
+   { 'postman-token': '4de78559-5319-522b-476e-656a5ac6ae68',
+     'cache-control': 'no-cache',
+     authorization: 'SSWS 00Xa5X9HjCcctwhj47zX0GMuX4edvxuQByjy4aO3UW',
+     'content-type': 'application/json',
+     accept: 'application/json' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+    res.render('applications', { title: 'Applications', body: body });
 });
+
+});
+
+
+
+
 
 module.exports = router;
