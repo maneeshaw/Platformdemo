9667015a8f8efdaf5867f63d068f0d14898e662c
Maneesha Wijesinghe <maneesha.wijesinghe@maneeshademo.com>
2016-11-29 00:25:53 -0500

    Third commit

--- routes/index.js
+++ routes/index.js
@@ -18,23 +18,6 @@ router.get('/signin', function(req, res, next) {
 // Registration
 
 router.get('/registration', function(req, res, next) {
-  var request = require("request");
-
-var options = { method: 'POST',
-  url: 'https://forest.okta.com/api/v1/users',
-  qs: { activate: 'true' },
-  headers: 
-   { 'postman-token': '7bc470a5-4789-cd7d-9588-14be8173aab0',
-     'cache-control': 'no-cache',
-     authorization: 'SSWS 00Xa5X9HjCcctwhj47zX0GMuX4edvxuQByjy4aO3UW',
-     'content-type': 'application/json',
-     accept: 'application/json' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
   res.render('registration', { title: 'Registration' });
 });
 
