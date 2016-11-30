2810e2d495fb1e0795ea0948fce617ee05cd2f18
Maneesha Wijesinghe <maneesha.wijesinghe@maneeshademo.com>
2016-11-30 00:13:40 -0500

    fgfhjk

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ console.log("hello");
    if (error || !response.ok) {
      console.log(error);
    } else {
-    var userId = res.id;
+    var userId = response.id;
     var query = "?userId=" + userId; 
     var redirectUrl = "/applications"
     res.redirect(redirectUrl + query);
